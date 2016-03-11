bec16d778de99e535656021d26f6ad76e3d8aa94
Evgeny Sinelnikov <sin@altlinux.ru>
2016-03-11 10:20:36 +0300

    Initial example based on BlinkWithoutDelay

--- intervals.ino
+++ intervals.ino
@@ -1,13 +1,18 @@
 const int ledPin = 13;
 const unsigned long interval = 1000;
 
+bool ledState = false;
+unsigned long previous;
+
 void setup() {
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(interval);
-  digitalWrite(ledPin, LOW);
-  delay(interval);
+  unsigned long current = millis();
+  if (current - previous >= interval) {
+    previous = current;
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
 }
